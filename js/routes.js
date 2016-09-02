4d62fb5f6087c6a9cb6bf18a40a94903241ace1d
RECEP UNCU <recepuncu@gmail.com>
2016-09-02 18:13:03 +0300

    initialize

---
+++ js/routes.js
@@ -0,0 +1,49 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('Lemler.araKabul', {
+    url: '/arac-kabul',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/araKabul.html',
+        controller: 'araKabulCtrl'
+      }
+    }
+  })
+
+  .state('Lemler.LemDetaylar', {
+    url: '/islem-detay',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/LemDetaylar.html',
+        controller: 'LemDetaylarCtrl'
+      }
+    }
+  })
+
+  .state('Lemler', {
+    url: '/side-menu-islemler',
+    templateUrl: 'templates/Lemler.html',
+    abstract:true
+  })
+
+  .state('oturumA', {
+    url: '/oturum-ac',
+    templateUrl: 'templates/oturumA.html',
+    controller: 'oturumACtrl'
+  })
+
+$urlRouterProvider.otherwise('/oturum-ac')
+
+  
+
+});
