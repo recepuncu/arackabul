f402c9a7405239dea9ef0f48c3f31e77f65c7003
RECEP UNCU <recepuncu@gmail.com>
2016-09-03 10:50:30 +0300

    a

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,7 @@ function ($scope, $stateParams, $cordovaCamera) {
 	
     $scope.takePicture = function() {
         var options = { 
-            quality : 75, 
+            quality : 100, 
             destinationType : Camera.DestinationType.DATA_URL, 
             sourceType : Camera.PictureSourceType.CAMERA, 
             allowEdit : false,
@@ -36,10 +36,10 @@ function ($scope, $stateParams) {
 
 }])
       
-.controller('oturumACtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('oturumACtrl', ['$scope', '$stateParams', '$cordovaDevice', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $cordovaDevice) {
 
 	//var uuid = $cordovaDevice.getUUID();
 	//alert( uuid );
