8026fe3b47e0f71050db8ecdbac9dbc833c4f9ed
RECEP UNCU <recepuncu@gmail.com>
2016-09-03 10:35:06 +0300

    a

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,30 @@
 angular.module('app.controllers', [])
   
-.controller('araKabulCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('araKabulCtrl', ['$scope', '$stateParams', '$cordovaCamera', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $cordovaCamera) {
+	
+    $scope.takePicture = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.CAMERA, 
+            allowEdit : false,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 350,
+            targetHeight: 350,
+            popoverOptions: CameraPopoverOptions,
+            correctOrientation: true,
+            saveToPhotoAlbum: false
+        };
 
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    };	
 
 }])
    
@@ -21,6 +41,8 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
-
+	var uuid = $cordovaDevice.getUUID();
+	alert( uuid );
+	
 }])
  
