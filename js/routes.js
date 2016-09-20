d6accc361636ec43fe9de99324a1f64b2b492643
RECEP UNCU <recepuncu@gmail.com>
2016-09-20 14:09:34 +0300

    a

--- js/routes.js
+++ js/routes.js
@@ -1,49 +1,47 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+	
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
+	// Ionic uses AngularUI Router which uses the concept of states
+	// Learn more here: https://github.com/angular-ui/ui-router
+	// Set up the various states which the app can be in.
+	// Each state's controller can be found in controllers.js
+	$stateProvider
 
-      .state('Lemler.araKabul', {
-    url: '/arac-kabul',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/araKabul.html',
-        controller: 'araKabulCtrl'
-      }
-    }
-  })
+	.state('Lemler.araKabul', {
+		url : '/arac-kabul',
+		views : {
+			'side-menu21' : {
+				templateUrl : 'templates/araKabul.html',
+				controller : 'araKabulCtrl'
+			}
+		}
+	})
 
-  .state('Lemler.LemDetaylar', {
-    url: '/islem-detay',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/LemDetaylar.html',
-        controller: 'LemDetaylarCtrl'
-      }
-    }
-  })
+	.state('Lemler.LemDetaylar', {
+		url : '/islem-detay',
+		views : {
+			'side-menu21' : {
+				templateUrl : 'templates/LemDetaylar.html',
+				controller : 'LemDetaylarCtrl'
+			}
+		}
+	})
 
-  .state('Lemler', {
-    url: '/side-menu-islemler',
-    templateUrl: 'templates/Lemler.html',
-    abstract:true
-  })
+	.state('Lemler', {
+		url : '/side-menu-islemler',
+		templateUrl : 'templates/Lemler.html',
+		abstract : true
+	})
 
-  .state('oturumA', {
-    url: '/oturum-ac',
-    templateUrl: 'templates/oturumA.html',
-    controller: 'oturumACtrl'
-  })
+	.state('oturumA', {
+		url : '/oturum-ac',
+		templateUrl : 'templates/oturumA.html',
+		controller : 'oturumACtrl'
+	})
 
-$urlRouterProvider.otherwise('/oturum-ac')
-
-  
+	$urlRouterProvider.otherwise('/oturum-ac')
 
 });
