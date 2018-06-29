37a224de5958e204aa7cc400c9fac3b804f8b011
RECEP UNCU <recepuncu@gmail.com>
2018-06-29 14:47:49 +0300

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -2,27 +2,28 @@ angular.module('app.controllers', [])
 
         .controller('araKabulCtrl', ['$scope', '$stateParams', '$cordovaCamera',
             function ($scope, $stateParams, $cordovaCamera) {
-
-                $scope.takePicture = function () {
-                    var options = {
-                        quality: 100,
-                        destinationType: Camera.DestinationType.DATA_URL,
-                        sourceType: Camera.PictureSourceType.CAMERA,
-                        allowEdit: false,
-                        encodingType: Camera.EncodingType.JPEG,
-                        targetWidth: 350,
-                        targetHeight: 350,
-                        popoverOptions: CameraPopoverOptions,
-                        correctOrientation: true,
-                        saveToPhotoAlbum: false
-                    };
-                    $cordovaCamera.getPicture(options).then(function (imageData) {
-                        $scope.imgURI = "data:image/jpeg;base64," + imageData;
-                    }, function (err) {
-                        // An error occured. Show a message to the user
-                    });
-                };
-
+				
+				document.addEventListener("deviceready", function () {
+					$scope.takePicture = function () {
+						var options = {
+							quality: 100,
+							destinationType: Camera.DestinationType.DATA_URL,
+							sourceType: Camera.PictureSourceType.CAMERA,
+							allowEdit: false,
+							encodingType: Camera.EncodingType.JPEG,
+							targetWidth: 350,
+							targetHeight: 350,
+							popoverOptions: CameraPopoverOptions,
+							correctOrientation: true,
+							saveToPhotoAlbum: false
+						};
+						$cordovaCamera.getPicture(options).then(function (imageData) {
+							$scope.imgURI = "data:image/jpeg;base64," + imageData;
+						}, function (err) {
+							// An error occured. Show a message to the user
+						});
+					};
+				}, false);
             }
         ])
 
